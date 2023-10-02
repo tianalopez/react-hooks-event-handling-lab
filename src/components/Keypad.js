7fb2fa62e299ebcd5625d220c57df3ae428eb514
Tiana Lopez <tianadavin@gmail.com>
2023-10-02 10:57:52 -0700

    Adds two new components

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,11 @@
 // Code Keypad Component Here
 
+
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={() => console.log("Entering password...")}></input>
+        </div>
     )
 }
 
